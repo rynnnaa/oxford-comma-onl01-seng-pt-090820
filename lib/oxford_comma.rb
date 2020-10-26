810357679b454afcb0fe4f1ebe2c2ffd4a4324de
Ryna Truong <rynatruong@outlook.com>
2020-10-26 21:56:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,10 @@ require 'pry'
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-    binding.pry
-  elsif
+    # binding.pry
+  else
     array.join(", ")
     array.insert(-2, " and")
-    
   end
 
 end
