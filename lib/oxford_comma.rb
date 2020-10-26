8d03db31f8f1f1abd610832b63a0ee4b392fbd8c
Ryna Truong <rynatruong@outlook.com>
2020-10-26 22:11:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+    # binding.pry
+  elsif array.length > 2
+    array.last.insert(0, "and ")
+    array.join(", ")
+        # binding.pry
+  else
+    array.join
+  end
 
 end
